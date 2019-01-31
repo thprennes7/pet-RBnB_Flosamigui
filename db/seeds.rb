212007caf090e881189e8df542797f400c14950e
Montblanc159 <guillaume.zaugg@gmail.com>
2019-01-31 17:35:40 +0100

    working DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,24 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'faker'
+
+Stroll.destroy_all
+Dog.destroy_all
+Dogsitter.destroy_all
+City.destroy_all
+
+15.times do
+  City.create!(city_name: Faker::Address.city)
+end
+
+50.times do
+  Dogsitter.create!(name: Faker::Name.name, city_id: City.all.sample.id)
+end
+
+50.times do
+  Dog.create!(name: Faker::Dog.name, city_id: City.all.sample.id)
+end
+
+150.times do
+  Stroll.create!(dog_id: Dogsitter.all.sample.id, dogsitter_id: Dog.all.sample.id, city_id: City.all.sample.id)
+end
